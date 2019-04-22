0e8be8e1ad1ce51bd389cbf05af5a5b764ee8ef9
Unknown <jessyrivard@icloud.com>
2019-04-22 18:40:56 -0400

    Changed Routers
    index router is now preceded by /api/, and users router has been removed for now

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 const MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
@@ -36,8 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
