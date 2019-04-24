ebab9e085dee751e5ad680adb74197fdc065a9f3
Unknown <jessyrivard@icloud.com>
2019-04-24 13:59:40 -0400

    Google Maps Api Data Save feature part 2
    Google maps data sent in an array using a JSON Content-type header will be taken in, the data checked against the database, then saved if not already existing. Process could be faster, but is async, so other things can be processed at the same time.
    
    Next step is to get the check request as one request for all the data in the array, rather than one request per object in the array, to save time and lower the amount of requests on the database.

--- providers/steakhouses.js
+++ providers/steakhouses.js
@@ -1,45 +1,66 @@
-'use strict'
-var mongoose = require('mongoose');
+"use strict";
+var mongoose = require("mongoose");
 
 //schema and model
-var Schema = mongoose.Schema
+var Schema = mongoose.Schema;
 var steakhouseSchema = new Schema({
   name: String,
   coordinates: {
     latitude: String,
     longitude: String
   }
-})
+});
 
-var Steakhouses = mongoose.model('steakhouses', steakhouseSchema)
+var Steakhouses = mongoose.model("steakhouses", steakhouseSchema);
 
 //queries and logic
 class steakhousesProvider {
-  constructor() { }
+  constructor() {}
 
   //find all steakhouses
   static findAll(callback) {
-    Steakhouses.find({}, function (err, steakhouses) {
+    Steakhouses.find({}, "name", function(err, steakhouses) {
       callback(null, steakhouses);
     });
   }
 
   //add steakhouses from maps API
   static addGoogleData(googleData, callback) {
-
-    //spread the array of data here, assuming frontend passes an array of objects
-    //recreate array (of objects) following schema
-
-    for (i = 0; i < googleData.length; i++) {
-      Steakhouses.updateOne(
-        { coordinates: { latitude: googleData[i].coordinates.latitude, longitude: googleData[i].coordinates.longitude} },
-        googleData[i],
-        options.upsert=true,
-        function (err, dataAmountAdded) {}
-      )
+    console.log(googleData.length + " steakhouses submitted to the database");
+    for (var i = 0; i < googleData.length; i++) {
+      var lat = googleData[i].coordinates.latitude;
+      var long = googleData[i].coordinates.longitude;
+      var steakhname = googleData[i].name;
+      Steakhouses.findOne(
+        {
+          coordinates: {
+            latitude: lat,
+            longitude: long
+          }
+        },
+        "name",
+        function(err, existingData) {
+          if (existingData === null) {
+            var steakhouseToSave = new Steakhouses({
+              name: steakhname,
+              coordinates: {
+                latitude: lat,
+                longitude: long
+              }
+            });
+            steakhouseToSave.save(function(err) {
+              console.log(steakhname + " has been saved");
+            });
+            return;
+          } else {
+            console.log(existingData.name + " is already in the database");
+            return;
+          }
+        }
+      );
     }
+    callback(null);
   }
 }
 
-
-exports.steakhousesProvider = steakhousesProvider
+exports.steakhousesProvider = steakhousesProvider;
